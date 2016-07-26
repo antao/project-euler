93dcccdc9e9fbe852a7c8906c8615d0240223ba8
Joao Antao <joaoantao@funda.nl>
2016-07-26 10:33:54 +0200

    problem/solution-007 - Implemented sieve of erastosthenes on the train this morning

--- python/007/problem-007.py
+++ python/007/problem-007.py
@@ -5,7 +5,6 @@
 # What is the 10 001st prime number?
 
 # Readme: https://en.wikipedia.org/wiki/Sieve_theory
-# Todo: Implement this https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 # https://en.wikipedia.org/wiki/Sieve_of_Sundaram
 # https://en.wikipedia.org/wiki/Sieve_of_Atkin
 
@@ -18,6 +17,19 @@ def prime(number):
             return False
     return True
 
+# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+def eratosthenes(limit):
+    print("Sieve of Erastosthenes will be executed with a limit number of %s" % limit)
+    composites = {}
+    counter = 0
+    for a in range(2, limit + 1):
+        if a not in composites.values():
+            print (a)
+            for j in range(a * a, limit + 1, a):
+                composites.update({counter: j})
+                counter += 1
+
+# Brute force solution
 def solution(number):
     primes = {}
     current = 2 
@@ -27,11 +39,16 @@ def solution(number):
             primes.update({counter: current}) 
             counter += 1
         current += 1 
-    print(primes)
+    # print(primes)
     return(primes.get(number - 1))
 
 if __name__ == '__main__':
-    start = time.time()
-    print (solution(10001))
-    end = (time.time() - start)
-    print ("Execution took %s seconds" % end)
+    # start = time.time()
+    # print (solution(50))
+    # end = (time.time() - start)
+    # print ("Execution took %s seconds" % end)
+
+    startEratosthenes = time.time()
+    eratosthenes(112)
+    endErastosthenees = (time.time() - startEratosthenes)
+    print ("Execution eratosthenes took %s seconds" % endErastosthenees)
