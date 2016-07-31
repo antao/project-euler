fdbd44616b5ebb25b86ef847aeda67d666efa79c
João Antão <joao.antao@protonmail.com>
2016-07-31 22:04:32 +0200

    problem/solution-010 - Improvement of prime()

--- python/010/problem-010.py
+++ python/010/problem-010.py
@@ -12,15 +12,19 @@ import math
 import time
 
 def prime(number): 
-    for a in range(2, number):
-        if number % a == 0:
+    if number % 2 == 0: 
+        return False
+    a = 3
+    while a < number ** 0.5 + 1:
+        if number % a == 0: 
             return False
+        a += 2
     return True
 
 # Brute force solution
 def solution(number):
-    primes = []
-    current = 2 
+    primes = [2]
+    current = 2
 
     while current < number + 1: 
         if prime(current):
